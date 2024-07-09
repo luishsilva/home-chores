cb79b61c0820f25bc753e50782369a37b106b159
luishsilva <luizhrick@gmail.com>
2024-07-09 07:47:03 -0600

    First commit, cleaning project

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <h1>HOME PAGE</h1>
+      <div className="card">
+        THIS IS THE HOME  PAGE
+      </div>
+    </>
+  )
+}
+
+export default App
