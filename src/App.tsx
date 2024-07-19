728194d6c40b2e0ba851686883d2e86f4918396c
luishsilva <luizhrick@gmail.com>
2024-07-19 08:37:17 -0600

    Install react-router and setup route links on NavBar

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,19 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { NavBar } from './components/NavBar';
 import { Home } from "./components/Home";
+import { Login } from "./components/Login";
 
 function App() {
 
   return (
-    <>
-      <Home />
-    </>
+    <Router>
+      <NavBar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+      </Routes>
+    </Router>
   );
 };
 
--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,19 +1,25 @@
+import { Link } from "react-router-dom";
+
 export const NavBar = () => {
     return (
         <nav className="navbar navbar-light bg-light px-4">
             <div className="container-fluid d-flex flex-row justify-content-between align-items-center">
-                <a className="navbar-brand" href="#">
-                    <img className="logo" src="../../public/house-hand.png" alt="logo" />
-                    <div className="ms-2">Home Chores</div>
-                </a>
-                <div className="align-items-center d-flex">
-                        <img className="user-thumb" src="default-user-thumb.png" alt="user profile thumbnail" />  
-                        <span className="ms-2">Login</span>
+                <Link className="navbar-brand" to="/">
+                    <div className="align-items-center d-flex">
+                        <img className="logo" src="../../public/house-hand.png" alt="logo" />
+                        <div className="ms-2">Home Chores</div>
                     </div>
-                    {/* 
-                        Will display if user is logged
-                        Signed in as: <a href="#login">Mark Otto</a> 
-                    */}
+                </Link>
+                <div className="align-items-center d-flex">
+                    <img className="user-thumb" src="default-user-thumb.png" alt="user profile thumbnail" />  
+                    <Link className="" to="/login">
+                        <span className="ms-2">Login</span>
+                    </Link>
+                </div>
+                {/* 
+                    Will display if user is logged
+                    Signed in as: <a href="#login">Mark Otto</a> 
+                */}
             </div>
         </nav>
     );
