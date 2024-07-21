e76294489e00b427fab663d8a8e735039cb3f9b7
luishsilva <luizhrick@gmail.com>
2024-07-21 10:30:05 -0600

    Add register component route

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { NavBar } from './components/NavBar';
 import { Home } from "./components/Home";
 import { Login } from "./components/Login";
+import { Register } from './components/Register';
 
 function App() {
 
@@ -12,6 +13,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
       </Routes>
     </Router>
   );
--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,3 +1,5 @@
+import { Link } from "react-router-dom";
+
 export const Home = () => (
     <div className='d-flex flex-column vh-100'>
         <div className="d-flex align-items-center justify-content-center flex-grow-1 flex-wrap-reverse">
@@ -6,11 +8,13 @@ export const Home = () => (
                     <div>Join Hands, Organize Together, Free Up Time</div>
                     <div>Everyone's Effort Counts!.</div>
                 </div>
-                <div className="d-grid col-6 mt-5 mx-auto">
-                    <button className="btn btn-primary" type="button">
-                        Create an account
-                    </button>
-                </div>
+                <Link to="/register">
+                    <div className="btn-create-account col-6 d-grid mt-5 mx-auto">
+                        <button className="btn btn-primary" type="button">
+                            Create an account
+                        </button>
+                    </div>
+                </Link>
             </div>
             <div className="d-flex justify-content-center align-items-center col-6 p-5">
                 <img className="home-img" src="/people-helping-2.png" alt="People Helping" />
