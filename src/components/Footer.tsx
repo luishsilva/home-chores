8db1e1fa929401d51918b66b833cbee653077629
luishsilva <luizhrick@gmail.com>
2024-07-22 08:18:34 -0600

    Add Footer component

---
+++ src/components/Footer.tsx
@@ -0,0 +1,8 @@
+export const Footer = () => {
+    const currentDate = new Date();
+    return (
+        <footer className="bg-light text-black text-center p-3">
+            copyright&copy; 2024-{currentDate.getFullYear()}
+        </footer>
+    );
+};
