5796bba60a6d0386c3de373508ba4e7c051ad8c7
luishsilva <luizhrick@gmail.com>
2024-08-14 23:24:18 -0600

    Rename Login component to SignIn

--- src/components/Dashboard.tsx
+++ src/components/Dashboard.tsx
@@ -1,11 +1,15 @@
-import React from 'react';
+import { useAuth } from '../context/AuthContext';
 
-const Dashboard = () => (
-  <main className="d-flex flex-column flex-grow-1">
-    <div className="d-flex align-items-center justify-content-center flex-grow-1 flex-wrap-reverse">
-      Dashboard
-    </div>
-  </main>
-);
+const Dashboard = () => {
+  const { user } = useAuth();
+
+  return (
+    <main className="d-flex flex-column flex-grow-1">
+      <div className="d-flex align-items-center justify-content-center flex-grow-1 flex-wrap-reverse">
+        Dashboard {user?.firstName}
+      </div>
+    </main>
+  );
+};
 
 export default Dashboard;
