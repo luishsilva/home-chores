3ebbed1a003f517963368b39692272497593e806
luishsilva <luizhrick@gmail.com>
2024-08-19 08:55:36 -0600

    Add chores route

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,10 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import NavBar from './components/NavBar';
 import Home from './components/Home';
 import SignIn from './components/SignIn';
-import Footer from './components/Footer';
 import SignUp from './components/SignUp';
 import Dashboard from './components/Dashboard';
+import Chores from './components/Chores';
 import { AuthProvider } from './context/AuthContext';
 
 const App = () => {
@@ -13,14 +12,13 @@ const App = () => {
     <div className="app">
       <Router>
         <AuthProvider>
-          <NavBar />
           <Routes>
             <Route path="/" element={<Home />} />
             <Route path="/login" element={<SignIn />} />
             <Route path="/signup" element={<SignUp />} />
             <Route path="/dashboard" element={<Dashboard />} />
+            <Route path="/chores" element={<Chores />} />
           </Routes>
-          <Footer />
         </AuthProvider>
       </Router>
     </div>
