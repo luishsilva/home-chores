744a384a57e61fd9b6bb742d7d1e5977d8a6779a
luishsilva <luizhrick@gmail.com>
2024-08-19 08:53:17 -0600

    Refactor home component remove SideBar and footer component

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,30 +1,36 @@
 import { Link } from 'react-router-dom';
+import NavBar from './NavBar';
+import Footer from './Footer';
 
 const Home = () => (
-  <main className="d-flex flex-column flex-grow-1">
-    <div className="d-flex align-items-center justify-content-center flex-grow-1 flex-wrap-reverse">
-      <div>
-        <div className="fs-2 font-weight-bold">
-          <div>Join Hands, Organize Together, Free Up Time</div>
-          <div>Everyone`&apos;s Effort Counts!.</div>
-        </div>
-        <Link to="/signup">
-          <div className="btn-create-account col-6 d-grid mt-5 mx-auto">
-            <button className="btn btn-primary" type="button">
-              Create an account
-            </button>
+  <>
+    <NavBar />
+    <main className="d-flex flex-column flex-grow-1">
+      <div className="d-flex align-items-center justify-content-center flex-grow-1 flex-wrap-reverse">
+        <div>
+          <div className="fs-2 font-weight-bold">
+            <div>Join Hands, Organize Together, Free Up Time</div>
+            <div>Everyone`&apos;s Effort Counts!.</div>
           </div>
-        </Link>
+          <Link to="/signup">
+            <div className="btn-create-account col-6 d-grid mt-5 mx-auto">
+              <button className="btn btn-primary" type="button">
+                Create an account
+              </button>
+            </div>
+          </Link>
+        </div>
+        <div className="d-flex justify-content-center align-items-center col-6 p-5">
+          <img
+            className="home-img"
+            src="/people-helping-2.png"
+            alt="People Helping"
+          />
+        </div>
       </div>
-      <div className="d-flex justify-content-center align-items-center col-6 p-5">
-        <img
-          className="home-img"
-          src="/people-helping-2.png"
-          alt="People Helping"
-        />
-      </div>
-    </div>
-  </main>
+    </main>
+    <Footer />
+  </>
 );
 
 export default Home;
