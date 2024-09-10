98c413538296e61559880b74d8fd7c93e0499288
luishsilva <luizhrick@gmail.com>
2024-09-10 15:14:06 -0600

    Add frequency array function file

---
+++ src/functions/choreFrequency.tsx
@@ -0,0 +1,24 @@
+const choreFrequency = [
+  {
+    id: '1',
+    title: 'Daily',
+  },
+  {
+    id: '2',
+    title: 'Weekly',
+  },
+  {
+    id: '3',
+    title: 'Bi-Weekly',
+  },
+  {
+    id: '4',
+    title: 'Monthly',
+  },
+  {
+    id: '5',
+    title: 'Seasonal/Annual',
+  },
+];
+
+export default choreFrequency;
